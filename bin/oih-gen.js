0b71e2615d3fcfdf10397b5495746bf0c189368a
Pavel Nedelko <pnedelko@gmail.com>
2023-05-29 16:26:45 +0200

    add pagination-type option

--- bin/oih-gen.js
+++ bin/oih-gen.js
@@ -8,7 +8,7 @@
  */
 
 const doGenerate = require("../lib/do-generate");
-const { program } = require("commander");
+const { program, Option } = require("commander");
 const { version } = require("../package.json");
 
 /**
@@ -19,15 +19,19 @@ async function oihGen() {
   program
     .version(version)
     .argument("<url or file>", "Url or file with OpenAPI definition")
-    .requiredOption(
-      "-o, --output <directory>",
-      "Output directory where a directory with a generated component will be created"
-    )
+    .requiredOption("-o, --output <directory>", "Output directory where a generated component will be created")
     .requiredOption("-n, --name <name>", "Component's name")
     .option("-s, --snapshot [string]", "Field name to be used for snapshot in triggers", "")
+    .addOption(
+      new Option("--pagination-type [type]", "Type of pagination")
+        .choices(["no_pagination", "cursor"])
+        .default("no_pagination")
+    )
     .parse();
 
   const options = program.opts();
+
+  // console.log(options);
 
   const url = program.args[0];
   if (!url) {
