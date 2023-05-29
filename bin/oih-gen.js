480c7e70a5a910ba7792cba3978f3939b80fc8fd
Pavel Nedelko <pnedelko@gmail.com>
2023-05-29 16:16:43 +0200

    use commander instead of getopts

--- bin/oih-gen.js
+++ bin/oih-gen.js
@@ -7,64 +7,46 @@
  * see the file LICENSE on the toplevel directory.
  */
 
-const getopts = require("getopts");
 const doGenerate = require("../lib/do-generate");
-const Questionnaire = require("../lib/questionnaire");
-
-module.exports = oihGen;
-
-oihGen().catch((e) => {
-  console.error("An error occurred.", e.message);
-  process.exit(1);
-});
+const { program } = require("commander");
+const { version } = require("../package.json");
 
 /**
  * CLI utility for generating a single connector
  * @returns {Promise<void>}
  */
 async function oihGen() {
-  const options = getopts(process.argv.slice(2), {
-    alias: {
-      output: "o",
-      name: "n",
-      help: "h",
-      yes: "y",
-      snapshot: "s",
-    },
-    string: ["o", "n", "s"],
-  });
+  program
+    .version(version)
+    .argument("<url or file>", "Url or file with OpenAPI definition")
+    .requiredOption(
+      "-o, --output <directory>",
+      "Output directory where a directory with a generated component will be created"
+    )
+    .requiredOption("-n, --name <name>", "Component's name")
+    .option("-s, --snapshot [string]", "Field name to be used for snapshot in triggers", "")
+    .parse();
 
-  if (options.help) {
-    printHelp();
-    return;
-  }
+  const options = program.opts();
 
-  const skipQuestionnaire = options.yes;
-  const q = new Questionnaire(skipQuestionnaire);
-
-  const url = options._[0];
+  const url = program.args[0];
   if (!url) {
-    printHelp();
     throw new Error(
-      "Missing required parameter. Please provide an url to download the swagger definition from or a path to a local file."
+      "Missing required parameter. Please provide an url to download the OpenAPI definition from or a path to a local file."
     );
   }
 
-  const outputDir = options.output || (await q.ask("Output directory", "output"));
-  await doGenerate({ swaggerUrl: url, connectorName: options.name, outputDir, snapshot: options.snapshot });
-
-  q.finish();
+  await doGenerate({
+    swaggerUrl: url,
+    connectorName: options.name,
+    outputDir: options.output,
+    snapshot: options.snapshot,
+  });
 }
 
-function printHelp() {
-  console.log(`
-        Usage: oih-gen [OPTIONS] {URL|FILE}
-        Usage example: oih-gen -o petstore-connector -n petstore https://petstore.swagger.io/v2/swagger.json\n
-        Generate a connector from an OpenAPI definition provided from an url or a local file.\n
-        Options:
-          -o, --output \t\t output directory where to store the downloaded and validated specification files and the generated connector
-          -n, --name \t\t connector name used as package name in package.json file
-          -y, --yes \t\t automatically populate all options with default values
-          -h, --help \t\t display this help
-    `);
-}
+oihGen().catch((e) => {
+  console.error("An error occurred.", e.message);
+  process.exit(1);
+});
+
+module.exports = oihGen;
