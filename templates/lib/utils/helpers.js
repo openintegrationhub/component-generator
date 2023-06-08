502db73b50decd0d5f8a15b802f55e5caa43b368
shulkaolka <olga.shukel@gmail.com>
2023-06-08 16:05:13 +0300

    Add Form-data processor

--- templates/lib/utils/helpers.js
+++ templates/lib/utils/helpers.js
@@ -1,5 +1,58 @@
 const dayjs = require("dayjs");
 const lodashGet = require("lodash.get");
+const fs = require("fs");
+const FormDataNode = require("formdata-node");
+const path = require("path");
+const axios = require("axios");
+const { File } = FormDataNode;
+
+const getFileName = (fileUrl) => {
+  const parsedUrl = new URL(fileUrl);
+  const filePath = parsedUrl.pathname;
+  const title = path.basename(filePath);
+  if (!title){
+    throw new Error("Cannot find filename in provided url");
+  }
+  return title;
+};
+
+const downloadFileFromUrl = async (fileUrl) => {
+  const title = getFileName(fileUrl);
+  const response = await axios({
+    method: "GET",
+    url: fileUrl,
+    responseType: "arraybuffer"
+  });
+  return new File([response.data], title);
+};
+
+const getInputMetadataSchema = (metadataPath) => {
+  const inputMetadata = fs.readFileSync(metadataPath, "utf-8");
+  return JSON.parse(inputMetadata).properties.requestBody.properties;
+};
+
+const mapFormDataBody = async function(action, body) {
+  this.logger.info("Going to import Input Metadata Schema...");
+  let inputMetadataSchema = getInputMetadataSchema(action.metadata.in);
+  this.logger.info("Input Metadata Schema: %j", inputMetadataSchema);
+  for (const key of Object.keys(body)) {
+    this.logger.info("Body property '%s' has type: %s", key, inputMetadataSchema[key].type);
+    if (inputMetadataSchema[key].type === "string" && inputMetadataSchema[key].format && inputMetadataSchema[key].format === "binary") {
+      this.logger.info("For body property '%s' detected 'binary' format. Going to download binary data from provided URL: %s", key, body[key]);
+      let fileUrl;
+      try{
+        fileUrl = new URL(body[key]);
+      } catch (e) {
+        this.logger.error("Body property '%s' has binary format and require valid URL as value, but has %s", key, body[key]);
+      }
+      this.logger.info("Going to download File from provided URL...");
+      const fileContent = await downloadFileFromUrl(fileUrl);
+      this.logger.info("File was successfully downloaded");
+      body[key] = fileContent;
+    }
+  }
+  return body;
+};
 
 function compareDate(a, b) {
   return dayjs(a).isAfter(b);
@@ -67,4 +120,5 @@ module.exports = {
   getMetadata,
   dataAndSnapshot,
   getElementDataFromResponse,
+  mapFormDataBody
 };
